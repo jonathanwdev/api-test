3eebc7dada6778b3ef8c595b2f290b60df851c42
Jonathan Souza <jonathan.wdev@gmail.com>
2020-08-27 16:58:23 -0300

    add updateProducts controller

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from 'express';
 import GetProductsService from '../services/GetProductsService';
+import UpdateProductsService from '../services/UpdateProductsService';
 
 const getProductsService = new GetProductsService();
+const updateProductsService = new UpdateProductsService();
 
 class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const products = await getProductsService.execute();
     return response.json(products);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { data } = request.body;
+    const updatedProducts = await updateProductsService.execute(data);
+    return response.json(updatedProducts);
   }
 }
 
